9db8be77613c067e86873e0f5504b5fa96d15cd3
Ultrafeeder <jakarimstratton@gmail.com>
2022-04-24 00:29:58 -0400

    editied data in projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@
     const desc =[{
         project_header:"Gateway Project",
         screenshot: require("../assets/seoul_travel.png"),
-        project_desc: "My father and Grnadmother are originally from Seoul, Korea, and I always wanted to travel there, so I designed this website for the Gateway program, which got me in to Road to Hire.",
+        project_desc: "My father and Grandmother are originally from Seoul, Korea, and I always wanted to travel there, so I designed this website for the Gateway program, which got me in to Road to Hire.",
         lang:"HTML/CSS",
         framework:"",
         link:"https://ultrafeeder.github.io/Seoul-Korea/",
@@ -11,7 +11,7 @@
     {
         project_header:"Ecommerce V1",
         screenshot:require("../assets/ecommerce.png"),
-        project_desc: "My father and Grnadmother are originally from Seoul, Korea, and I always wanted to travel there, so I designed this website for the Gateway program, which got me in to Road to Hire.",
+        project_desc: "A website for a digital marketplace. I love illustration, and still do it in my spare time, and this project was built to showcase that hobby.",
         lang:"HTML/CSS",
         framework:"",
         link:"https://ultrafeeder.github.io/Ecommerce/",
@@ -20,7 +20,7 @@
     {
         project_header:"Ecommerce React",
         screenshot:require("../assets/ecommerce_react.png"),
-        project_desc: "My father and Grnadmother are originally from Seoul, Korea, and I always wanted to travel there, so I designed this website for the Gateway program, which got me in to Road to Hire.",
+        project_desc: "A later version of the original Ecommerce project. This is also meant to showcase my illustration but now incorporates JavaScript and React.",
         lang:"HTML/CSS/JavaScript/SQL",
         framework:"React",
         link:"https://www.janusmarketplace.com",
@@ -28,7 +28,7 @@
     },
     {
         project_header:"Code For Justice Hackathon",        "screenshot":require("../assets/ladder_rungs.png"),
-        project_desc: "My father and Grnadmother are originally from Seoul, Korea, and I always wanted to travel there, so I designed this website for the Gateway program, which got me in to Road to Hire.",
+        project_desc: "This project was a group effort used to promote a service we would like to see implemented in our society. The service we chose was a financial counseling service owned by the fictional 'Ladder Rungs Association'. My part was completely designing the Impact page.",
         lang:"HTML/CSS",
         framework:"",
         link:"https://ultrafeeder.github.io/Group-6/",
@@ -37,7 +37,7 @@
     {
         project_header:"Tesla Clone",
         screenshot:require("../assets/tesla_clone.png"),
-        project_desc: "My father and Grnadmother are originally from Seoul, Korea, and I always wanted to travel there, so I designed this website for the Gateway program, which got me in to Road to Hire.",
+        project_desc: "A group project where we were tasked with redesigning a popular website. Our group chose the Tesla website and my part was designing a nav bar and a reusable solar component for the solar roofing/solar paneling sections of the website.",
         lang:"HTML/CSS/JavaScript/SQL",
         framework:"React/AWS",
         link:"http://dylisthebest.c9tesla.com",
