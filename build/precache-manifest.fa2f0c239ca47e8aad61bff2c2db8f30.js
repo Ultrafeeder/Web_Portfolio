7c43ab99444d4bb93c35bdc670de9c8b6566b9b3
Ultrafeeder <jakarimstratton@gmail.com>
2022-04-25 09:10:30 -0400

    made build

---
+++ build/precache-manifest.fa2f0c239ca47e8aad61bff2c2db8f30.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "70092d33cb3d7f05b457",
+    "url": "/static/css/main.372279d1.chunk.css"
+  },
+  {
+    "revision": "70092d33cb3d7f05b457",
+    "url": "/static/js/main.70092d33.chunk.js"
+  },
+  {
+    "revision": "6ed4e863b6eb731f89af",
+    "url": "/static/js/1.6ed4e863.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bcf86fbd5b7233a0947f36807ad8ebd8",
+    "url": "/static/media/seoul_travel.bcf86fbd.png"
+  },
+  {
+    "revision": "82bf2f29f710c5308ead3dad8b25e817",
+    "url": "/static/media/ecommerce.82bf2f29.png"
+  },
+  {
+    "revision": "9cc382b4b63c17fdd76662a8932bd39d",
+    "url": "/static/media/ecommerce_react.9cc382b4.png"
+  },
+  {
+    "revision": "174adba7a3b0cb2b35bfa34a97a2de6a",
+    "url": "/static/media/ladder_rungs.174adba7.png"
+  },
+  {
+    "revision": "cd13eadb41b4a5b2a02edcaad8315896",
+    "url": "/static/media/tesla_clone.cd13eadb.png"
+  },
+  {
+    "revision": "49279c95ab6587c6cde092f30fe8034f",
+    "url": "/static/media/head_shot.49279c95.JPG"
+  },
+  {
+    "revision": "04d4e6f7e85417261bb916a250a84721",
+    "url": "/static/media/JakariStratton-logos.04d4e6f7.jpeg"
+  },
+  {
+    "revision": "50013ec04357411af6fd66a6b866c393",
+    "url": "/static/media/x.50013ec0.png"
+  },
+  {
+    "revision": "71d831fd928d2b02d3f4df8e312a6b8c",
+    "url": "/index.html"
+  }
+];
